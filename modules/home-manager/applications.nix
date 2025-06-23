b72ad168dd1940a721b421a60ee0ca36282ddd9b
Igor Bedesqui <igorbedesqui@gmail.com>
2025-06-23 12:19:16 +0100

    add spotify cli, remove linux specific, update transmission

--- modules/home-manager/applications.nix
+++ modules/home-manager/applications.nix
@@ -2,33 +2,24 @@
 
 {
   home.packages = with pkgs; [
-    # Password management
     _1password-gui
     _1password-cli
 
-    # Development tools
     docker
     blockbench
     tableplus
 
-    # Media and entertainment  
     iina
     obsidian
     prismlauncher
-    transmission-gtk
+    spicetify-cli
+    spotify-player
+    transmission_4
 
     # Note: Apps not available or broken on current nixpkgs:
     # - ghostty (marked as broken)
     # - obs-studio (not available on macOS in nixpkgs)
     # - figma-linux (Linux-specific)
-    # - spotify (may require different package)
-    # - steam (may require different package)
     # - linear-linear, notion-calendar (not found in nixpkgs)
-  ] ++ lib.optionals (!pkgs.stdenv.isDarwin) [
-    # Linux-only packages
-    figma-linux
-    obs-studio
-    spotify
-    steam
   ];
 }
