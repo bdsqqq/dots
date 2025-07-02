bde8d766c8974b4cfd8fda7437d22bf61f010285
Igor Bedesqui <igorbedesqui@gmail.com>
2025-07-02 13:48:12 +0100

    Add nushell system-level support for nix-darwin
    - Created modules/darwin/nushell.nix with system-level nushell configuration
    - Added nushell to /etc/shells and system packages
    - Fixed shell aliases for nushell compatibility
    - Removed non-existent enableNushellIntegration option from fzf

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./karabiner.nix
+    ./nushell.nix
   ];
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
@@ -25,7 +26,7 @@
   nix.settings.experimental-features = "nix-command flakes";
 
   # Enable alternative shell support in nix-darwin.
-  # programs.fish.enable = true;
+  programs.nushell.enable = true;
 
   # Set Git commit hash for darwin-version.
   # system.configurationRevision = self.rev or self.dirtyRev or null;
