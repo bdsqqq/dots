8009c3ad9c430b6b67269f28aa7aacc6fe1f3180
Igor Bedesqui <igorbedesqui@gmail.com>
2025-07-03 21:02:49 +0100

    add floating lazygit to neovim! home-grown organic no pluginsss

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -24,6 +24,46 @@
         },
       }
       vim.cmd.colorscheme('vesper')
+
+      -- floating terminal function
+      local function open_floating_term(cmd)
+        local columns = vim.api.nvim_get_option("columns")
+        local lines = vim.api.nvim_get_option("lines")
+
+        local width = math.floor(columns * 0.8)
+        local height = math.floor(lines * 0.8)
+        if width < 60 then width = columns end
+        if height < 20 then height = lines end
+        local row = math.floor((lines - height) / 2)
+        local col = math.floor((columns - width) / 2)
+
+        local buf = vim.api.nvim_create_buf(false, true)
+        local opts = {
+          style = "minimal",
+          relative = "editor",
+          width = width,
+          height = height,
+          row = row,
+          col = col,
+          border = "rounded",
+        }
+        local win = vim.api.nvim_open_win(buf, true, opts)
+
+        vim.fn.termopen(cmd, {
+          on_exit = function()
+            if vim.api.nvim_win_is_valid(win) then
+              vim.api.nvim_win_close(win, true)
+            end
+            if vim.api.nvim_buf_is_valid(buf) then
+              vim.api.nvim_buf_delete(buf, {force=true})
+            end
+          end
+        })
+        vim.cmd("startinsert")
+      end
+
+      -- make function globally available
+      _G.open_floating_term = open_floating_term
     '';
 
     globals = {
@@ -248,6 +288,17 @@
         action = "<cmd>set rnu!<CR>";
         options.desc = "[t]oggle [r]elative numbers";
       }
+      # floating lazygit terminal
+      {
+        mode = "n";
+        key = "<leader>tg";
+        action.__raw = ''
+          function()
+            _G.open_floating_term('lazygit')
+          end
+        '';
+        options.desc = "[t]oggle [g]it (lazygit)";
+      }
     ];
 
     autoGroups = {
@@ -293,6 +344,7 @@
       stylua
       nodePackages.prettier
       go
+      lazygit
     ];
 
     plugins = {
