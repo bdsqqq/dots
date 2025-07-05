82f08ddfeacd7d25cec2d559167339c14a92c50a
Igor Bedesqui <igorbedesqui@gmail.com>
2025-07-05 21:55:18 +0100

    import stuff into desktop

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -96,58 +96,18 @@
     useUserPackages = true;
     
     users.bdsqqq = {
+      imports = [
+        ../../modules/home-manager/profiles/niri.nix
+        ../../modules/home-manager/applications.nix
+        ../../modules/home-manager/development.nix
+        ../../modules/home-manager/neovim.nix
+        ../../modules/home-manager/shell.nix
+      ];
+      
       home = {
         username = "bdsqqq";
         homeDirectory = "/home/bdsqqq";
         stateVersion = "25.05";
-        
-        # Basic niri config
-        file.".config/niri/config.kdl".text = ''
-          input {
-              keyboard {
-                  xkb {
-                      layout "us"
-                  }
-              }
-          }
-          
-          layout {
-              gaps 16
-              
-              border {
-                  width 2
-              }
-          }
-          
-          binds {
-              Mod+Return { spawn "foot"; }
-              Mod+D { spawn "fuzzel"; }
-              Mod+Q { close-window; }
-              
-              Mod+H { focus-column-left; }
-              Mod+L { focus-column-right; }
-              Mod+J { focus-window-down; }
-              Mod+K { focus-window-up; }
-              
-              Mod+1 { focus-workspace 1; }
-              Mod+2 { focus-workspace 2; }
-              Mod+3 { focus-workspace 3; }
-              Mod+4 { focus-workspace 4; }
-              Mod+5 { focus-workspace 5; }
-          }
-          
-          spawn-at-startup "waybar"
-          spawn-at-startup "mako"
-        '';
-        
-        packages = with pkgs; [
-          foot
-          fuzzel
-          waybar
-          mako
-          firefox
-          vscode
-        ];
       };
     };
     
