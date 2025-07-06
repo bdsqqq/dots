be20fdb38e3e88ec161492492c03ae029da491ea
Igor Bedesqui <igorbedesqui@gmail.com>
2025-07-06 16:06:03 -0400

    fix audio on monitor

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -93,6 +93,31 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+    
+    # WirePlumber configuration for default HDMI audio
+    wireplumber = {
+      enable = true;
+      configPackages = [
+        (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/51-hdmi-default.conf" ''
+          monitor.alsa.rules = [
+            {
+              matches = [
+                {
+                  device.name = "~alsa_card.pci-0000_*_00.1"
+                }
+              ]
+              actions = {
+                update-props = {
+                  api.alsa.use-acp = true
+                  device.profile.switch = true
+                  device.profile = "output:hdmi-stereo"
+                }
+              }
+            }
+          ]
+        '')
+      ];
+    };
   };
 
   # Security
