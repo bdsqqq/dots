def8c7cf42ea1b6b20f6cdc4fdfd9542791be4cf
Igor Bedesqui <igorbedesqui@gmail.com>
2025-07-14 17:12:37 +0100

    pnpm global packages

---
+++ pnpm-global-packages.nix
@@ -0,0 +1,22 @@
+# Global pnpm packages configuration
+# Add packages you want available globally here
+{
+  # Development tools
+  typescript = "^5.0.0";
+  prettier = "^3.0.0";
+  eslint = "^8.0.0";
+  
+  # Build tools
+  vite = "^5.0.0";
+  
+  # Utilities
+  nodemon = "^3.0.0";
+  
+  # Package managers and tools
+  npm-check-updates = "^16.0.0";
+  
+  # Example of how to add more packages:
+  # "package-name" = "version-spec";
+  # turbo = "^1.0.0";
+  # "@types/node" = "^20.0.0";
+}
