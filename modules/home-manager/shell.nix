334531a03fbcb854f3e4720137cd437d8caacaa2
Igor Bedesqui <igorbedesqui@gmail.com>
2025-09-14 05:10:23 +0100

    make shell not suck, turns out oh my zsh is useless

--- modules/home-manager/shell.nix
+++ modules/home-manager/shell.nix
@@ -138,24 +138,34 @@
   programs = {
     zsh = {
       enable = true;
-      oh-my-zsh = {
-        enable = true;
-        theme = "vercel";
-        plugins = [ "git" ];
-        custom = "$HOME/.config/zsh/oh-my-zsh-custom";
-      };
-      plugins = [
-        {
-          name = "zsh-autosuggestions";
-          src = pkgs.fetchFromGitHub {
-            owner = "zsh-users";
-            repo = "zsh-autosuggestions";
-            rev = "v0.7.0";
-            sha256 = "1g3pij5qn2j7v7jjac2a63lxd97mcsgw6xq6k5p7835q9fjiid98";
-          };
-        }
-      ];
+      # oh-my-zsh = {
+      #   enable = true;
+      #   theme = "vercel";
+      #   plugins = [ "git" ];
+      #   custom = "$HOME/.config/zsh/oh-my-zsh-custom";
+      # };
+      # plugins = [
+      #   {
+      #     name = "zsh-autosuggestions";
+      #     src = pkgs.fetchFromGitHub {
+      #       owner = "zsh-users";
+      #       repo = "zsh-autosuggestions";
+      #       rev = "v0.7.0";
+      #       sha256 = "1g3pij5qn2j7v7jjac2a63lxd97mcsgw6xq6k5p7835q9fjiid98";
+      #     };
+      #   }
+      # ];
       initExtra = ''
+        # Lazy load completions (major performance win)
+        autoload -Uz compinit
+        () {
+          if [[ $# -gt 0 ]]; then
+            compinit -C
+          else
+            compinit -C -d "$HOME/.zcompdump-''${HOST}-''${ZSH_VERSION}"
+          fi
+        } ''${ZDOTDIR:-$HOME}/.zcompdump(N.mh+24)
+        
         # Show hidden files by default in shell globbing
         setopt GLOB_DOTS
         
@@ -163,6 +173,18 @@
         export FZF_DEFAULT_COMMAND='rg --files --hidden --follow'
         export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
         export FZF_DEFAULT_OPTS='--height=40% --layout=reverse --border'
+        
+        # Simple fast prompt (inspired by vercel theme)
+        autoload -U colors && colors
+        setopt PROMPT_SUBST
+        
+        # Git branch function (much faster than oh-my-zsh version)
+        git_branch() {
+          git symbolic-ref --short HEAD 2>/dev/null | sed 's/^/[/' | sed 's/$/]/'
+        }
+        
+        PROMPT='%{$fg_bold[white]%}△ %c%{$reset_color%}$(git_branch)
+ %{$fg_bold[white]%}↳ %{$reset_color%}'
       '';
       shellAliases = {
         # Add your custom aliases here
@@ -181,57 +203,7 @@
         "--cmd cd"  # Replace cd with zoxide's z command
       ];
     };
+    
   };
 
-  # Custom files
-  home.file.".config/zsh/oh-my-zsh-custom/themes/vercel.zsh-theme".text = ''
-    local resetColor="%{$reset_color%}"
-    local logo="%{$fg_bold[white]%}△$resetColor$resetColor"
-    local hostname=`hostname`
-    local dir="%{$fg_bold[white]%}%c$resetColor$resetColor"
-    local inputIndicator=" %{$fg_bold[White]%}↳ $resetColor"
-
-    GIT_PROMPT_PREFIX="[%{$fg_bold[white]%}"
-    GIT_PROMPT_SUFFIX="$resetColor]"
-    GIT_PROMPT_DIRTY="%{$fg_bold[lightWhite]%}"
-    GIT_PROMPT_CLEAN="%{$fg_bold[darkWhite]%}"
-
-    # modified from https://github.com/robbyrussell/oh-my-zsh/blob/576ada138fc5eed3f58a4aff8141e483310c90fb/lib/git.zsh#L12
-    function branch_is_dirty() {
-      local STATUS=""
-      local -a FLAGS
-      FLAGS=('--porcelain')
-      if [[ "$(command git config --get oh-my-zsh.hide-dirty)" != "1" ]]; then
-        if [[ $POST_1_7_2_GIT -gt 0 ]]; then
-          FLAGS+='--ignore-submodules=dirty'
-        fi
-        if [[ "$DISABLE_UNTRACKED_FILES_DIRTY" == "true" ]]; then
-          FLAGS+='--untracked-files=no'
-        fi
-        STATUS=$(command git status ''${FLAGS} 2> /dev/null | tail -n1)
-      fi
-      if [[ -n $STATUS ]]; then
-        return 0
-      else
-        return 1
-      fi
-    }
-
-    function git_prompt() {
-      branch=`git_current_branch`
-      if [ "$branch" = "" ]; then
-        # not a git repo
-        echo ""
-      else
-        if branch_is_dirty; then
-          echo "$GIT_PROMPT_PREFIX$GIT_PROMPT_DIRTY$branch$GIT_PROMPT_SUFFIX"
-        else
-          echo "$GIT_PROMPT_PREFIX$GIT_PROMPT_CLEAN$branch$GIT_PROMPT_SUFFIX"
-        fi
-      fi
-    }
-    NEWLINE=$'\n '
-
-    PROMPT='$logo $dir $(git_prompt) $NEWLINE $inputIndicator'
-  '';
 }
