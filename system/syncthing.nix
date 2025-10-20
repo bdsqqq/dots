88e3dfa1a3161aed459ad9f9b1de8e5e9ee5d596
Igor Bedesqui <igorbedesqui@gmail.com>
2025-10-20 18:34:08 +0100

    fix(syncthing): use config.nixpkgs.hostPlatform instead of pkgs.stdenv to avoid recursion

--- system/syncthing.nix
+++ system/syncthing.nix
@@ -1,33 +1,35 @@
-{ lib, pkgs, ... }:
+{ lib, config, pkgs, ... }:
 let
-  cfg = {
-    linux = lib.optionalAttrs pkgs.stdenv.isLinux {
-      services.syncthing = {
-        enable = true;
-        user = "bdsqqq";
-        dataDir = "/home/bdsqqq";
-        configDir = "/home/bdsqqq/.config/syncthing";
-      };
-    };
-    
-    darwin = lib.optionalAttrs pkgs.stdenv.isDarwin {
-      launchd.user.agents.syncthing = {
-        serviceConfig = {
-          Label = "com.bdsqqq.syncthing";
-          ProgramArguments = [
-            "${pkgs.syncthing}/bin/syncthing"
-            "-no-browser"
-            "-home"
-            "/Users/bdsqqq/.config/syncthing"
-          ];
-          WorkingDirectory = "/Users/bdsqqq";
-          RunAtLoad = true;
-          KeepAlive = true;
-          StandardOutPath = "/Users/bdsqqq/Library/Logs/syncthing.log";
-          StandardErrorPath = "/Users/bdsqqq/Library/Logs/syncthing.log";
-        };
-      };
-    };
-  };
+  inherit (lib) mkIf mkMerge;
+  system = config.nixpkgs.hostPlatform.system or builtins.currentSystem;
+  isLinux = lib.hasInfix "linux" system;
+  isDarwin = lib.hasInfix "darwin" system;
 in
-cfg.linux // cfg.darwin
+mkMerge [
+  (mkIf isLinux {
+    services.syncthing = {
+      enable = true;
+      user = "bdsqqq";
+      dataDir = "/home/bdsqqq";
+      configDir = "/home/bdsqqq/.config/syncthing";
+    };
+  })
+  (mkIf isDarwin {
+    launchd.user.agents.syncthing = {
+      serviceConfig = {
+        Label = "com.bdsqqq.syncthing";
+        ProgramArguments = [
+          "${pkgs.syncthing}/bin/syncthing"
+          "-no-browser"
+          "-home"
+          "/Users/bdsqqq/.config/syncthing"
+        ];
+        WorkingDirectory = "/Users/bdsqqq";
+        RunAtLoad = true;
+        KeepAlive = true;
+        StandardOutPath = "/Users/bdsqqq/Library/Logs/syncthing.log";
+        StandardErrorPath = "/Users/bdsqqq/Library/Logs/syncthing.log";
+      };
+    };
+  })
+]
