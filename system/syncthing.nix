6bfee22824608261be130ebfdff31e06d4745898
Igor Bedesqui <igorbedesqui@gmail.com>
2025-10-20 18:35:12 +0100

    fix(syncthing): use if-else at module level to truly skip platform-specific options

--- system/syncthing.nix
+++ system/syncthing.nix
@@ -1,35 +1,31 @@
 { lib, config, pkgs, ... }:
 let
-  inherit (lib) mkIf mkMerge;
   system = config.nixpkgs.hostPlatform.system or builtins.currentSystem;
   isLinux = lib.hasInfix "linux" system;
   isDarwin = lib.hasInfix "darwin" system;
 in
-mkMerge [
-  (mkIf isLinux {
-    services.syncthing = {
-      enable = true;
-      user = "bdsqqq";
-      dataDir = "/home/bdsqqq";
-      configDir = "/home/bdsqqq/.config/syncthing";
+if isLinux then {
+  services.syncthing = {
+    enable = true;
+    user = "bdsqqq";
+    dataDir = "/home/bdsqqq";
+    configDir = "/home/bdsqqq/.config/syncthing";
+  };
+} else if isDarwin then {
+  launchd.user.agents.syncthing = {
+    serviceConfig = {
+      Label = "com.bdsqqq.syncthing";
+      ProgramArguments = [
+        "${pkgs.syncthing}/bin/syncthing"
+        "-no-browser"
+        "-home"
+        "/Users/bdsqqq/.config/syncthing"
+      ];
+      WorkingDirectory = "/Users/bdsqqq";
+      RunAtLoad = true;
+      KeepAlive = true;
+      StandardOutPath = "/Users/bdsqqq/Library/Logs/syncthing.log";
+      StandardErrorPath = "/Users/bdsqqq/Library/Logs/syncthing.log";
     };
-  })
-  (mkIf isDarwin {
-    launchd.user.agents.syncthing = {
-      serviceConfig = {
-        Label = "com.bdsqqq.syncthing";
-        ProgramArguments = [
-          "${pkgs.syncthing}/bin/syncthing"
-          "-no-browser"
-          "-home"
-          "/Users/bdsqqq/.config/syncthing"
-        ];
-        WorkingDirectory = "/Users/bdsqqq";
-        RunAtLoad = true;
-        KeepAlive = true;
-        StandardOutPath = "/Users/bdsqqq/Library/Logs/syncthing.log";
-        StandardErrorPath = "/Users/bdsqqq/Library/Logs/syncthing.log";
-      };
-    };
-  })
-]
+  };
+} else {}
