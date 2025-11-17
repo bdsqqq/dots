b8f66ff301712261f86003e7cbaa4fa46b7b0263
Igor Bedesqui <igorbedesqui@gmail.com>
2025-11-17 21:00:35 +0000

    update sops key

--- system/sops.nix
+++ system/sops.nix
@@ -1,30 +1,22 @@
-{ lib, inputs, hostSystem ? null, config, ... }:
+{ lib, inputs, hostSystem ? null, ... }:
 let
   isDarwin = lib.hasInfix "darwin" hostSystem;
-  isLinux = lib.hasInfix "linux" hostSystem;
   
-  keyPath = if isDarwin 
-    then "/Users/bdsqqq/.config/sops/age/keys.txt"
-    else "/var/lib/sops-nix/key.txt";
-  
-  keyExists = builtins.pathExists keyPath;
+  sshKeyPath = if isDarwin 
+    then "/Users/bdsqqq/.ssh/id_ed25519"
+    else "/home/bdsqqq/.ssh/id_ed25519";
 in
 {
   sops = {
-    age.keyFile = keyPath;
-    defaultSopsFile = inputs.self + "/secrets.yaml";
+    # use user ssh key for decryption
+    age.sshKeyPaths = [ sshKeyPath ];
     
-    # only declare secrets if key exists, otherwise they'll fail to decrypt
-    secrets = lib.mkIf keyExists {
+    defaultSopsFile = inputs.self + "/secrets.yaml";
+    secrets = {
       anthropic_api_key = { };
       tailscale_auth_key = { };
     };
   };
-  
-  # warn if key file doesn't exist
-  warnings = lib.optional 
-    (!keyExists)
-    "sops age key file not found at ${keyPath} - secrets will not be available. run: sudo mkdir -p $(dirname ${keyPath}) && ssh-to-age -private-key -i ~/.ssh/id_ed25519 | sudo tee ${keyPath}";
 }
 
 
