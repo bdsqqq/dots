5ce5c3fadc01411e091968a4291f1752db27568d
Igor Bedesqui <igorbedesqui@gmail.com>
2025-11-17 20:23:28 +0000

    Revert "make age key optional"
    This reverts commit 2b3f4e9296f2b197f01c029b06992f75a26c2afb.

--- system/sops.nix
+++ system/sops.nix
@@ -1,18 +1,14 @@
 { lib, inputs, hostSystem ? null, ... }:
-let
-  isDarwin = lib.hasInfix "darwin" hostSystem;
-  keyFile = if isDarwin 
-    then "/Users/bdsqqq/.config/sops/age/keys.txt"
-    else "/var/lib/sops-nix/key.txt";
-  
-  keyExists = builtins.pathExists keyFile;
-in
 {
-  warnings = lib.optional (!keyExists) 
-    "sops age key not found at ${keyFile} - secrets will not be decrypted. run ssh-to-age to generate key from ssh key.";
+  # common sops-nix settings; key location differs per OS
+  sops = {
+    age = if lib.hasInfix "darwin" hostSystem then {
+      keyFile = "/Users/bdsqqq/.config/sops/age/keys.txt";
+    } else {
+      keyFile = "/var/lib/sops-nix/key.txt";
+    };
 
-  sops = lib.mkIf keyExists {
-    age.keyFile = keyFile;
+    # default encrypted file and concrete secrets mapping (flake root)
     defaultSopsFile = inputs.self + "/secrets.yaml";
     secrets = {
       anthropic_api_key = { };
