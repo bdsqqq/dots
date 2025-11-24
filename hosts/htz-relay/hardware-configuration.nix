03f9778582bdd9ba74b17662f2c1e856c16b1dba
Igor Bedesqui <igorbedesqui@gmail.com>
2025-11-24 19:11:29 +0000

    htz-relay: mount storage-01 volume

--- hosts/htz-relay/hardware-configuration.nix
+++ hosts/htz-relay/hardware-configuration.nix
@@ -5,5 +5,11 @@
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
   boot.initrd.kernelModules = [ "nvme" ];
   fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
+
+  fileSystems."/mnt/storage-01" = {
+    device = "/dev/disk/by-id/scsi-0HC_Volume_104047902";
+    fsType = "ext4";
+    options = [ "discard" "defaults" "nofail" ];
+  };
   
 }
