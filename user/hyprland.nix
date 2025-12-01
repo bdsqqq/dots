37f11608db396622d0f6e9a96a72e118d1b621d6
Igor Bedesqui <igorbedesqui@gmail.com>
2025-12-01 20:00:17 +0000

    feat(r56): 8px rounded corners, workspaces in waybar (zellij-style)

--- user/hyprland.nix
+++ user/hyprland.nix
@@ -43,13 +43,13 @@ if !(lib.hasInfix "linux" hostSystem) then {} else {
       general = {
         gaps_in = 4;
         gaps_out = 8;
-        border_size = 2;
+        border_size = 0;
         layout = "dwindle";
         resize_on_border = true;
       };
       
       decoration = {
-        rounding = 0;
+        rounding = 8;
         blur.enabled = false;
       };
       
