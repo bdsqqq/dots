b1ff1645a65fb4cac8295078d2780c0174fcaade
Igor Bedesqui <igorbedesqui@gmail.com>
2025-12-05 19:51:34 +0000

    fix(r56): add XDG_DATA_DIRS for gsettings schemas in toggle-theme

--- user/hyprland.nix
+++ user/hyprland.nix
@@ -3,6 +3,7 @@
 let
   toggleTheme = pkgs.writeShellScriptBin "toggle-theme" ''
     export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus"
+    export XDG_DATA_DIRS="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}''${XDG_DATA_DIRS:+:$XDG_DATA_DIRS}"
     current=$(${pkgs.glib}/bin/gsettings get org.gnome.desktop.interface color-scheme)
     if [ "$current" = "'prefer-dark'" ]; then
       ${pkgs.glib}/bin/gsettings set org.gnome.desktop.interface color-scheme prefer-light
