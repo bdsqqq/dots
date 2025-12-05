7a65430b86ca99f47fe2efe7680068f07278cf60
Igor Bedesqui <igorbedesqui@gmail.com>
2025-12-05 20:15:12 +0000

    fix(rg): ignore steam/flatpak dirs with broken symlinks

--- user/fzf.nix
+++ user/fzf.nix
@@ -3,9 +3,7 @@
   home-manager.users.bdsqqq = { config, pkgs, ... }: {
     programs.fzf = {
       enable = true;
-      # removed --follow: steam/flatpak dirs have broken symlinks that peg cpu
-      # added globs to skip problematic dirs entirely
-      defaultCommand = "rg --files --hidden --glob '!.steam' --glob '!.local/share/flatpak' --glob '!.local/share/Steam'";
+      defaultCommand = "rg --files --hidden --follow";
       defaultOptions = [
         "--height=40%"
         "--layout=reverse"
@@ -16,7 +14,7 @@
     programs.zsh.initExtra = ''
       # custom fzf file widget (ctrl+f)
       _fzf_files() {
-        local selected=$(rg --files --hidden --glob '!.steam' --glob '!.local/share/flatpak' --glob '!.local/share/Steam' | fzf --height=40% --layout=reverse --border)
+        local selected=$(rg --files --hidden --follow | fzf --height=40% --layout=reverse --border)
         LBUFFER="''${LBUFFER}''${selected}"
         zle reset-prompt
       }
