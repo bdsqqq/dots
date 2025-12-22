d0e713a47774440e410436bbd02efa324fd505af
igor bedesqui <igorbedesqui@gmail.com>
2025-12-22 19:37:41 +0000

    refactor(gaming): dedupe prismlauncher as cross-platform

--- user/gaming.nix
+++ user/gaming.nix
@@ -2,17 +2,13 @@
 
 let
   isDarwin = lib.hasInfix "darwin" hostSystem;
-  isLinux = lib.hasInfix "linux" hostSystem;
 in
 {
   home-manager.users.bdsqqq = { pkgs, ... }: {
-    home.packages = 
-      lib.optionals isDarwin [
-        pkgs.modrinth-app
-        pkgs.prismlauncher
-      ] ++
-      lib.optionals isLinux [
-        pkgs.prismlauncher
-      ];
+    home.packages = [
+      pkgs.prismlauncher
+    ] ++ lib.optionals isDarwin [
+      pkgs.modrinth-app
+    ];
   };
 }
