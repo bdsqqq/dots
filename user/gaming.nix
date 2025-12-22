9f089fcec4511935faa1d91d2b1c24ae018df625
igor bedesqui <igorbedesqui@gmail.com>
2025-12-22 19:07:06 +0000

    fix(gaming): symlink flatpak modrinth data to syncthing path

--- user/gaming.nix
+++ user/gaming.nix
@@ -5,9 +5,16 @@ let
   isLinux = lib.hasInfix "linux" hostSystem;
 in
 {
-  home-manager.users.bdsqqq = { pkgs, ... }: {
+  home-manager.users.bdsqqq = { pkgs, config, ... }: {
     home.packages = lib.optionals isDarwin [
       pkgs.modrinth-app
     ];
+    
+    # flatpak modrinth uses ~/.var/app/.../data/ModrinthApp but syncthing syncs to ~/.local/share/ModrinthApp
+    # symlink so flatpak reads the synced data
+    home.file = lib.mkIf isLinux {
+      ".var/app/com.modrinth.ModrinthApp/data/ModrinthApp".source =
+        config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.local/share/ModrinthApp";
+    };
   };
 }
