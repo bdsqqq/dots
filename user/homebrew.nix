ceed3eadf2cc7c29988047cf65b768c4723a2427
Igor Bedesqui <igorbedesqui@gmail.com>
2026-02-13 14:11:52 +0000

    chore(homebrew): replace deprecated `brewprefix` with `prefix` option

--- user/homebrew.nix
+++ user/homebrew.nix
@@ -1,17 +1,17 @@
 { lib, hostSystem ? null, config, ... }:
 let
   isDarwin = lib.hasInfix "darwin" hostSystem;
-  brewPrefix = if isDarwin then config.homebrew.brewPrefix or "/opt/homebrew" else "";
+  prefix = if isDarwin then config.homebrew.prefix or "/opt/homebrew" else "";
 in
 {
   home-manager.users.bdsqqq = { config, pkgs, ... }: {
     programs.zsh.initContent = lib.mkIf isDarwin ''
       # homebrew shellenv (darwin only)
-      eval "$(${brewPrefix}/brew shellenv)"
+      eval "$(${prefix}/brew shellenv)"
     '';
     programs.bash.initExtra = lib.mkIf isDarwin ''
       # homebrew shellenv (darwin only)
-      eval "$(${brewPrefix}/brew shellenv)"
+      eval "$(${prefix}/brew shellenv)"
     '';
   };
 }
