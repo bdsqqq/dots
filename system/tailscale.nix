236cb3563f2aa7a3140a74d3cd4a05e9ed370b80
Igor Bedesqui <igorbedesqui@gmail.com>
2026-03-19 21:41:27 +0000

    tailscale: use homemanager ts, not app

--- system/tailscale.nix
+++ system/tailscale.nix
@@ -1,16 +1,12 @@
 { lib, hostSystem ? null, ... }:
 let
   isLinux = hostSystem != null && lib.hasInfix "linux" hostSystem;
-in
-if isLinux then {
+in {
   services.tailscale = {
     enable = true;
+  } // lib.optionalAttrs isLinux {
     extraUpFlags = lib.mkDefault [ "--ssh" ];
   };
-} else {
-  # on darwin, use the mac app store tailscale app instead of nix-darwin's tailscaled.
-  # the app store app bundles its own daemon via network extension and provides
-  # better macOS integration. enabling both causes race conditions on startup.
 }
 
 
