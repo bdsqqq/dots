7c47e18b5134a10f479837355c7ba94e336f48b4
Igor Bedesqui <igorbedesqui@gmail.com>
2026-05-02 21:15:25 +0100

    fix(go): avoid modernize package collision
    Session-Id: 019dea27-a586-724e-9b31-c6c8e783a09b

--- user/go.nix
+++ user/go.nix
@@ -1,13 +1,22 @@
 { ... }: {
-  home-manager.users.bdsqqq = { pkgs, ... }: {
-    home.packages = with pkgs; [
-      go
-      gopls # LSP
-      gofumpt # formatter
-      golangci-lint # linter
-      gotools # goimports, etc.
-      gotests # test generator
-      delve # debugger
-    ];
-  };
+  home-manager.users.bdsqqq = { pkgs, ... }:
+    let
+      gotoolsWithoutModernize = pkgs.symlinkJoin {
+        name = "gotools-without-modernize";
+        paths = [ pkgs.gotools ];
+        postBuild = ''
+          rm "$out/bin/modernize"
+        '';
+      };
+    in {
+      home.packages = with pkgs; [
+        go
+        gopls # LSP
+        gofumpt # formatter
+        golangci-lint # linter
+        gotoolsWithoutModernize # goimports, etc.; gopls also ships modernize
+        gotests # test generator
+        delve # debugger
+      ];
+    };
 }
