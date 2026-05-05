a749bede384e6712b7e44211e7df17447adb473c
Igor Bedesqui <igorbedesqui@gmail.com>
2026-05-05 12:44:31 +0100

    feat(nh): default system subcommand
    Session-Id: 019df780-249a-752c-a835-2330ef05259d

--- system/nh.nix
+++ system/nh.nix
@@ -7,13 +7,22 @@ let
       "/Users/bdsqqq/commonplace/01_files/nix"
     else
       "/home/bdsqqq/commonplace/01_files/nix";
+  systemSubject = if isDarwin then "darwin" else "os";
+  nhWrapped = pkgs.writeShellScriptBin "nh" ''
+    real="${pkgs.nh}/bin/nh"
+    case "''${1:-}" in
+      os|darwin|home|clean|search|-*|--*|"") exec "$real" "$@" ;;
+      *) exec "$real" "${systemSubject}" "$@" ;;
+    esac
+  '';
 in
 if isDarwin then {
-  environment.systemPackages = [ pkgs.nh ];
+  environment.systemPackages = [ nhWrapped ];
   environment.variables.NH_FLAKE = flakePath;
 } else {
   programs.nh = {
     enable = true;
+    package = nhWrapped;
     flake = flakePath;
   };
 }
