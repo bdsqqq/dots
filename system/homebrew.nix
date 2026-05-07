773f6dee90b1f9310074822965afe42183b47982
Igor Bedesqui <igorbedesqui@gmail.com>
2026-05-07 12:02:27 +0100

    fix(homebrew): pass no-api env during activation
    Session-Id: 019e0207-053f-776f-ac9e-a003daf2fb3c

--- system/homebrew.nix
+++ system/homebrew.nix
@@ -1,9 +1,24 @@
-{ lib, hostSystem ? null, ... }:
+{ config, lib, pkgs, hostSystem ? null, ... }:
 
 if !(lib.hasInfix "darwin" hostSystem) then
   { }
 else {
-  environment.variables.HOMEBREW_NO_INSTALL_FROM_API = "1";
+  # nix-darwin does not preserve arbitrary Homebrew env vars through sudo.
+  system.activationScripts.homebrew.text = lib.mkForce ''
+    # Homebrew Bundle
+    echo >&2 "Homebrew bundle..."
+    if [ -f "${config.homebrew.prefix}/bin/brew" ]; then
+      PATH="${config.homebrew.prefix}/bin:${lib.makeBinPath [ pkgs.mas ]}:$PATH" \
+      sudo \
+        --preserve-env=PATH \
+        --user=${lib.escapeShellArg config.homebrew.user} \
+        --set-home \
+        env HOMEBREW_NO_INSTALL_FROM_API=1 \
+        ${config.homebrew.onActivation.brewBundleCmd}
+    else
+      echo -e "\e[1;31merror: Homebrew is not installed, skipping...\e[0m" >&2
+    fi
+  '';
 
   homebrew = {
     enable = true;
