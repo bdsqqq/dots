837b41419e5c86fb80a96a2a082c283d27430afd
Igor Bedesqui <igorbedesqui@gmail.com>
2026-05-09 10:26:19 +0100

    feat(ssh): enable other macs

--- system/ssh.nix
+++ system/ssh.nix
@@ -8,6 +8,13 @@ else {
     settings = {
       PermitRootLogin = "no";
       PasswordAuthentication = lib.mkDefault false;
+      Macs = [
+        "hmac-sha2-512-etm@openssh.com"
+        "hmac-sha2-256-etm@openssh.com"
+        "umac-128-etm@openssh.com"
+        "hmac-sha2-512"
+        "hmac-sha2-256"
+      ];
     };
   };
 }
