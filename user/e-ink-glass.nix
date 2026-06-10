52509dd1b6168583e3f93256364dc3da8def8f5a
Igor Bedesqui <igorbedesqui@gmail.com>
2026-06-10 20:01:13 +0100

    try to fix failing builds by making my kvantum thingy override config it finds

--- user/e-ink-glass.nix
+++ user/e-ink-glass.nix
@@ -7,10 +7,13 @@ in lib.mkIf (isLinux && headMode == "graphical") {
       themeDir = "${config.home.homeDirectory}/commonplace/01_files/nix/user/themes/e-ink-glass";
     in {
       xdg.configFile = {
-        "Kvantum/kvantum.kvconfig".text = ''
-          [General]
-          theme=EInkGlass
-        '';
+        "Kvantum/kvantum.kvconfig" = {
+          text = ''
+           [General]
+           theme=EInkGlass
+          '';
+          override = true;
+        };
         "Kvantum/EInkGlass" = {
           source = config.lib.file.mkOutOfStoreSymlink "${themeDir}/Kvantum/EInkGlass";
           recursive = true;
