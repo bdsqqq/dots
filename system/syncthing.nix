5c3a929ccd68232ca5698bc858eb7b2bade2322f
Igor Bedesqui <igorbedesqui@gmail.com>
2026-06-24 21:29:36 +0100

    feat(syncthing): make commonplace not shit itself if outside home dir on linux

--- system/syncthing.nix
+++ system/syncthing.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, hostSystem ? null, ... }:
+{ lib, pkgs, config, hostSystem ? null, ... }:
 let
   system = hostSystem;
   isLinux = lib.hasInfix "linux" system;
@@ -6,17 +6,47 @@ let
   syncthing = import ../modules/syncthing.nix { inherit lib; };
   stignore = pkgs.writeText "commonplace-stignore"
     (syncthing.mkStignore (builtins.readFile ../config/ignore-common));
-  userSyncthingConfig = {
-    home-manager.users.bdsqqq = { lib, config, ... }: {
-      home.activation.commonplaceStignore =
-        lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-          mkdir -p "${config.home.homeDirectory}/commonplace"
-          rm -f "${config.home.homeDirectory}/commonplace/.stignore"
-          install -m 0644 ${stignore} "${config.home.homeDirectory}/commonplace/.stignore"
-        '';
-    };
+
+  linuxCommonplacePath = lib.attrByPath [
+    "services"
+    "syncthing"
+    "settings"
+    "folders"
+    "commonplace"
+    "path"
+  ] "/home/bdsqqq/commonplace"
+    config;
+
+  linuxStignoreConfig = {
+    systemd.tmpfiles.rules = [
+      "d ${linuxCommonplacePath} 0700 bdsqqq users -"
+      "C+ ${linuxCommonplacePath}/.stignore 0644 bdsqqq users - ${stignore}"
+    ];
   };
-in if isLinux then lib.recursiveUpdate userSyncthingConfig {
+
+  darwinStignoreConfig = {
+    home-manager.users.bdsqqq = { lib, config, ... }:
+      let
+        commonplacePath = lib.attrByPath [
+          "services"
+          "syncthing"
+          "settings"
+          "folders"
+          "commonplace"
+          "path"
+        ] "${config.home.homeDirectory}/commonplace"
+          config;
+      in
+      {
+        home.activation.commonplaceStignore =
+          lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+            mkdir -p ${lib.escapeShellArg commonplacePath}
+            rm -f ${lib.escapeShellArg "${commonplacePath}/.stignore"}
+            install -m 0644 ${stignore} ${lib.escapeShellArg "${commonplacePath}/.stignore"}
+          '';
+      };
+  };
+in if isLinux then lib.recursiveUpdate linuxStignoreConfig {
   # NixOS uses system service; declarative folder/device config in host files
   services.syncthing = {
     enable = true;
@@ -29,7 +59,7 @@ in if isLinux then lib.recursiveUpdate userSyncthingConfig {
   networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ 8384 22000 ];
   networking.firewall.interfaces."tailscale0".allowedUDPPorts = [ 22000 21027 ];
 } else if isDarwin then
-  userSyncthingConfig // {
+  darwinStignoreConfig // {
     # darwin: syncthing managed entirely by home-manager's services.syncthing
     # (creates launchd agents for both daemon and config init)
   }
