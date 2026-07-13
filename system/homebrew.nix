3a6bc884705591d439ed1461228014c074660e33
Igor Bedesqui <igorbedesqui@gmail.com>
2026-07-12 22:59:54 -0300

    fix(homebrew): remove untrusted cask
    Session-Id: 019f53bd-1218-7edc-b903-7be645730e21

--- system/homebrew.nix
+++ system/homebrew.nix
@@ -3,53 +3,11 @@
 if !(lib.hasInfix "darwin" hostSystem) then
   { }
 else
-  let
-    homebrewConfigHome = "/Users/${config.homebrew.user}/.config";
-    homebrewTrustJson = builtins.toJSON {
-      trustedtaps = [ "pluk-inc/tap" ];
-      trustedcasks = [ "pluk-inc/tap/markdown-preview" ];
-    };
-    homebrewTrustFile = pkgs.writeText "homebrew-trust.json" homebrewTrustJson;
-  in
   {
-    # nix-darwin does not preserve arbitrary Homebrew env vars through sudo.
-    system.activationScripts.homebrew.text = lib.mkForce ''
-      # Homebrew Bundle
-      echo >&2 "Homebrew bundle..."
-      if [ -f "${config.homebrew.prefix}/bin/brew" ]; then
-        homebrew_config_home=${lib.escapeShellArg homebrewConfigHome}
-        mkdir -p "$homebrew_config_home/homebrew"
-        chown ${lib.escapeShellArg config.homebrew.user}:staff "$homebrew_config_home" "$homebrew_config_home/homebrew"
-        rm -f "$homebrew_config_home/homebrew/trust.json"
-        ln -s ${homebrewTrustFile} "$homebrew_config_home/homebrew/trust.json"
-        chown -h ${lib.escapeShellArg config.homebrew.user}:staff "$homebrew_config_home/homebrew/trust.json"
-
-        export PATH="${config.homebrew.prefix}/bin:${lib.makeBinPath [ pkgs.mas ]}:$PATH"
-        sudo \
-          --preserve-env=PATH \
-          --user=${lib.escapeShellArg config.homebrew.user} \
-          --set-home \
-          env \
-            HOMEBREW_NO_INSTALL_FROM_API=1 \
-            XDG_CONFIG_HOME=${lib.escapeShellArg homebrewConfigHome} \
-            ${config.homebrew.onActivation.brewBundleCmd { onlyCheck = false; }}
-      else
-        echo -e "\e[1;31merror: Homebrew is not installed, skipping...\e[0m" >&2
-      fi
-    '';
-
-    home-manager.users.bdsqqq.xdg.configFile."homebrew/trust.json" = {
-      source = homebrewTrustFile;
-      force = true;
-    };
-
     homebrew = {
       enable = true;
 
-      taps = [
-        "homebrew/cask"
-        "pluk-inc/tap"
-      ];
+      taps = [ "homebrew/cask" ];
 
       casks = [
         # System utilities
@@ -66,7 +24,6 @@ else
 
         # Productivity applications
         "linear"
-        "markdown-preview"
         "notion-calendar"
         "notion"
 
