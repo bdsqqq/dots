a070174906d1443665374b94e3386040dae4ae0a
Igor Bedesqui <igorbedesqui@gmail.com>
2026-08-26 22:18:46 -0300

    fix(ssh): install authorized keys as regular files
    Session-Id: 01a03e5d-13c2-787f-9fd6-b680ddfc8471

--- modules/ssh/authorized-keys.nix
+++ modules/ssh/authorized-keys.nix
@@ -9,8 +9,20 @@ if isLinux then
   }
 else
   {
-    home-manager.users.bdsqqq.home.file.".ssh/authorized_keys" = {
-      force = true;
-      text = lib.concatStringsSep "\n" (sshKeys.personalKeys ++ sshKeys.openDisplayPortalKeys) + "\n";
+    # OpenSSH StrictModes rejects Home Manager's usual symlink because its
+    # path traverses the group-writable Nix store. Keep the source declarative,
+    # but atomically install a user-owned regular file for sshd to read.
+    home-manager.users.bdsqqq = { lib, pkgs, ... }:
+    let
+      authorizedKeys = pkgs.writeText "authorized_keys" (
+        lib.concatStringsSep "\n" (sshKeys.personalKeys ++ sshKeys.openDisplayPortalKeys) + "\n"
+      );
+    in
+    {
+      home.activation.installAuthorizedKeys = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        $DRY_RUN_CMD ${pkgs.coreutils}/bin/install -d -m 0700 "$HOME/.ssh"
+        $DRY_RUN_CMD ${pkgs.coreutils}/bin/install -m 0600 ${authorizedKeys} "$HOME/.ssh/.authorized_keys.new"
+        $DRY_RUN_CMD ${pkgs.coreutils}/bin/mv -f "$HOME/.ssh/.authorized_keys.new" "$HOME/.ssh/authorized_keys"
+      '';
     };
   }
