008f769ada2e9f8346fe59c0e5766ff23b4c0e92
Igor Bedesqui <igorbedesqui@gmail.com>
2026-09-01 17:18:20 -0300

    fix(ghostty): link mutable config files individually
    Session-Id: 01a05dda-d76a-7947-910b-162d410a45f6

--- modules/ghostty/default.nix
+++ modules/ghostty/default.nix
@@ -4,12 +4,22 @@ let
   isDarwin = lib.hasInfix "darwin" hostSystem;
   isLinux = lib.hasInfix "linux" hostSystem;
   repoConfig = "${config.my.paths.commonplace}/01_files/nix/modules/ghostty/config";
+  configFiles = [
+    "config"
+    "themes/compline-dark"
+    "themes/lauds-light"
+    "themes/vesper-dark"
+    "themes/vesper-light"
+  ];
   ghosttyHome = {
     home-manager.users.bdsqqq = { config, ... }: {
-      xdg.configFile."ghostty" = {
-        source = config.lib.file.mkOutOfStoreSymlink repoConfig;
-        force = true;
-      };
+      xdg.configFile = lib.genAttrs
+        (map (name: "ghostty/${name}") configFiles)
+        (name: {
+          source = config.lib.file.mkOutOfStoreSymlink
+            "${repoConfig}/${lib.removePrefix "ghostty/" name}";
+          force = true;
+        });
     };
   };
 
